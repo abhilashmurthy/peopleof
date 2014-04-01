6df6cd46a094d063f81a8eea2de5b2085973d7d7
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2014-04-02 03:23:22 +0800

    Finished basic building of network using FQL Need to add loading sign when building network Need to organize circles

--- client/client.js
+++ client/client.js
@@ -12,7 +12,9 @@ Template.navbar.events = {
 Template.user_loggedout.events({
   'click #login': function(e) {
     $('.login2').show();
-    Meteor.loginWithFacebook(function(err) {
+    Meteor.loginWithFacebook({
+      requestPermissions: ['user_education_history', 'friends_education_history', 'user_work_history', 'friends_work_history']
+    }, function(err) {
       if (err) console.log(err);
       else console.log('Logged in!');
     });
@@ -31,6 +33,8 @@ Template.user_loggedin.events({
 // Place
 
 Template.place.place = function() {
+  var aPlace = Places.findOne();
+  Session.set('place', aPlace && aPlace.facebookId);
   return Places.findOne();
 }
 
@@ -61,6 +65,7 @@ Template.map.members = function() {
 Template.member.circle = function() {
   var member = this; //cache this shit
   var radius = 60;
+  var selectorId = "#" + member._id;
 
   existing_circles = d3.select("#members").selectAll("g");
   console.log("current # of things = " + existing_circles.size());
@@ -73,7 +78,7 @@ Template.member.circle = function() {
   x_next = x_next + (x_increment * existing_circles.size());
 
   setTimeout(function() {
-    circle = d3.select("#members").selectAll("#" + member._id)
+    circle = d3.select("#members").selectAll(selectorId);
     circle_data = circle.data([member._id]);
     g_container = circle_data.enter()
       .append("g")
@@ -132,101 +137,23 @@ d3.selection.prototype.size = function() {
   return n;
 };
 
-// Template.map.rendered = function () {
-//   var self = this;
-//   self.node = self.find("svg#memberCircles");
-//   self.pics = self.find("svg#profilePics");
-
-//   if (! self.handle) {
-//     self.handle = Deps.autorun(function () {
-//       var selected = Session.get('selected');
-//       var selectedMember = selected && Meteor.users.findOne(selected);
-//       var radius = 60;
-
-//       //Fill each circle with user's profile pic
-//       // var updatePatterns = function (group) {
-//       //   group
-//       //   .attr("id", function (member) { return "pattern_" + member._id; })
-//       //   .attr("heigth", 1)
-//       //   .attr("width", 1)
-//       //   .attr("patternUnits", "userSpaceOnUse")
-//       //   .append("image")
-//       //   .attr("id", function (member) {return member._id;})
-//       //   .attr("x", 0)
-//       //   .attr("y", 0)
-//       //   .attr("xlink:href", function (member) {return "http://graph.facebook.com/" + member.services.facebook.id + "/picture?type=large";})
-//       // };
-
-//       // var patterns = d3.select(self.pics).select("#picDefs").selectAll("pattern")
-//       //   .data(Meteor.users.find().fetch(), function (member) { return member._id; });
-
-//       // updatePatterns(patterns.enter().append("pattern"));
-//       // updatePatterns(patterns.transition().duration(250).ease("cubic-out"));
-//       // patterns.exit().remove();
-
-//       // Draw a circle for each member
-//       var updateCircles = function (group) {
-//         group
-//         .attr("id", function (member) { return member._id; })
-//         .attr("cx", function (member) { return member.x})        
-//         .attr("cy", function (member) { return member.y})
-//         .attr("r", radius)
-//         .style({
-//          "opacity": function (member) {
-//              return selected === member._id ? 1 : 0.6;
-//          }
-//         });
-//       };
-
-//       var circles = d3.select(self.node).select(".circles").selectAll("circle")
-//         .data(Meteor.users.find().fetch(), function (member) { return member._id; });
-
-//       updateCircles(circles.enter().append("circle"));
-//       updateCircles(circles.transition().duration(250).ease("cubic-out"));
-//       circles.exit().transition().duration(250).attr("r", 0).remove();
-
-//       // Label each with the current attendance count
-//       var updateLabels = function (group) {
-//         group.attr("id", function (member) { return member._id; })
-//         .text(function (member) {return member.services.facebook.name;})
-//         .attr("x", function (member) { return member.x; })
-//         .attr("y", function (member) { return member.y })
-//         .style('font-size', function (member) {
-//           return 14 + "px";
-//         });
-//       };
-
-//       var labels = d3.select(self.node).select(".labels").selectAll("text")
-//         .data(Meteor.users.find().fetch(), function (member) { return member._id; });
-
-//       updateLabels(labels.enter().append("text"));
-//       updateLabels(labels.transition().duration(250).ease("cubic-out"));
-//       labels.exit().remove();
-
-//       // Draw a dashed circle around the currently selected member, if any
-//       // var callout = d3.select(self.node).select("circle.callout")
-//       //   .transition().duration(250).ease("cubic-out");
-//       // if (selectedmember)
-//       //   callout.attr("cx", selectedmember.x)
-//       //   .attr("cy", selectedmember.y)
-//       //   .attr("r", 40)
-//       //   .attr("class", "callout")
-//       //   .attr("display", '');
-//       // else
-//       //   callout.attr("display", 'none');
-//     });
-//   }
-// };
-// 
-// Template.map.destroyed = function () {
-//   this.handle && this.handle.stop();
-// };
-
 //////////////////////////////////////////////////////////////////////////////////////////////// DETAILS
 Template.details.selected = function() {
   var member_id = Session.get('selected');
   if (member_id) return Members.findOne(member_id);
 }
+
+Template.details.events({
+  'click button#build': function (e) {
+    var placeId = Session.get('place');
+    Meteor.call('testNetwork', placeId, function (err, data) {
+      if (err) console.log(err);
+    })
+  },
+  'click button#clear': function (e) {
+
+  }
+});
 
 //////////////////////////////////////////////////////////////////////////////////////////////// PLUGINS
 //Pnotify settings
--- model.js
+++ model.js
@@ -12,11 +12,33 @@ var ConvertedNumber = Match.Where(function(x) {
 });
 
 Meteor.methods({
-	createMember: function(fbData) {
+	createMember: function (fbData) {
 		Members.insert({
 			id: fbData.id,
 			name: fbData.name,
 			username: fbData.username
 		});
+	},
+	testNetwork: function (placeId) {
+		var fbId = Meteor.user().services.facebook.id;
+		var query = "SELECT uid,name,username FROM user WHERE uid IN (SELECT uid1 FROM friend WHERE uid2=me()) AND placeId IN education";
+		query = query.replace(/ /g, '+');
+		query = query.replace('placeId', placeId);
+        var friendResult = Meteor.http.get(
+            "https://graph.facebook.com/fql"
+            + "?q=" + query
+            + "&access_token=" + Meteor.user().services.facebook.accessToken
+        );
+        var friendsInPlace = friendResult.data ? friendResult.data.data : null;
+        console.log(friendsInPlace.length);
+        if (!friendsInPlace) return false;
+        for (var i = 0; i < 5; i++) {
+        	Meteor.call('createMember', {
+        		id: friendsInPlace[i].uid,
+        		name: friendsInPlace[i].name,
+        		username: friendsInPlace[i].username
+        	});
+        }
+        return true;
 	}
 });
